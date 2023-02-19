2d4af8184531dfad266e011500e68bd900c64ea7
Martin Severin Steffensen <maritim@gmail.com>
2023-02-19 20:55:02 +0100

    Refactored one of the frequency quizzes to be based on number inputs.

--- src/routes/frequencies-1/+page.svelte
+++ src/routes/frequencies-1/+page.svelte
@@ -1,7 +1,49 @@
 <script lang="ts">
     import { page } from "$app/stores";
-    import Quiz from "$lib/components/Quiz.svelte";
-    import Wavelength from "$lib/components/Wavelength.svelte";
+    import type { Answer } from "$lib/answer";
+    import type { Question } from "./+page";
+    
+    let answers: Answer[] = [];
+    let start: number | undefined;
+    let end: number | undefined;
+    let currentId = $page.data.questions[0].id;
+    let isCompleted = false;
+    let nextButton: HTMLButtonElement;
+
+    function submit(): void {
+		if (currentId != $page.data.questions[$page.data.questions.length - 1].id) {
+            start = undefined;
+            end = undefined;
+			currentId++;
+		} else {
+			isCompleted = true;
+		}
+	}
+
+    function answer(): void {
+        if(!start) {
+            alert('Angi nedre grense.');
+        } else if(!end) {
+            alert('Angi øvre grense.');
+        } else {
+            answers = [
+                ...answers.filter((answer) => answer.id !== currentQuestion.id),
+                {
+                    id: currentQuestion.id,
+                    value: currentQuestion.answer.start === start && currentQuestion.answer.end === end
+                },
+            ];
+            nextButton.focus();
+        }
+    }
+
+    function focus(element: HTMLInputElement): void {
+        element.focus();
+    }
+
+    $: currentQuestion = $page.data.questions.find((question: Question) => question.id === currentId) as Question;
+    $: currentAnswer = answers.find((a) => a.id === currentId);
+    $: score = answers.reduce((sum, a) => sum += a.value ? 1 : 0, 0);
 </script>
 
 <svelte:head>
@@ -18,6 +60,58 @@
             </div>
         </div>
     </div>
-    <Wavelength />
-    <Quiz questions={$page.data.questions} />
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2>Fyll inn korrekt nedre og øvre grense for båndet.</h2>
+                <div class="card">
+                    <h5 class="card-header">Spørsmål {$page.data.questions.indexOf(currentQuestion) + 1} av {$page.data.questions.length}: {currentQuestion.question}</h5>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            {#if currentAnswer}
+                                <div class="alert alert-{currentAnswer.value ? 'success' : 'danger'}">
+                                    <h4 class="alert-heading">{currentAnswer.value ? 'Riktig' : 'Feil'} svar!</h4>
+                                    <p>Du svarte at frekvensområdet for dette båndet er {start} - {end} kHz.</p>
+                                    <hr>
+                                    <p class="mb-0">Fasiten er {currentQuestion.answer.start} - {currentQuestion.answer.end} kHz.</p>
+                                </div>
+                                {#if currentQuestion.explanation}
+                                    <div class="alert alert-info mt-2" role="alert">
+                                        <h4 class="alert-heading">Forklaring</h4>
+                                        <p class="mb-0">{currentQuestion.explanation}</p>
+                                    </div>
+                                {/if}
+                            {:else}
+                                <form on:submit|preventDefault={answer}>
+                                    <div class="row mb-3">
+                                        <label for="start" class="col-sm-2 col-form-label">Nedre grense (MHz)</label>
+                                        <div class="col-sm-10">
+                                            <input type="number" id="start" class="form-control" bind:value={start} use:focus>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <label for="end" class="col-sm-2 col-form-label">Øvre grense (MHz)</label>
+                                        <div class="col-sm-10">
+                                            <input type="number" id="end" class="form-control" bind:value={end}>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Sjekk</button>
+                                </form>
+                            {/if}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-2">
+            <div class="col">
+                Poeng: {score}
+            </div>
+            <div class="col text-end">
+                {#if currentId !== $page.data.questions[$page.data.questions.length - 1].id}
+                    <button class="btn btn-primary" on:click={submit} bind:this={nextButton}>Neste</button>
+                {/if}
+            </div>
+        </div>
+    </div>
 </section>
--- src/routes/frequencies-1/+page.ts
+++ src/routes/frequencies-1/+page.ts
@@ -1,56 +1,94 @@
-import type { Question } from "$lib/question";
+export type Question = {
+    id: number;
+    question: string;
+    answer: {
+        start: number;
+        end: number;
+    },
+    explanation: string;
+};
 
 export function load() {
     const explanation = 'Formelen for å beregne ca. frekvens (MHz) er f = 300 / λ.';
     const questions: Question[] = [{
         id: 1,
-        question: 'Hva er allokeringen(e) for 160 meter-båndet?',
-        answer: '1810 - 2000 kHz, 1850 - 2000 er sekundært',
-        explanation: explanation
+        question: 'Hva er allokeringen(e) for 160 meter-båndet i kHz?',
+        answer: {
+            start: 1810,
+            end: 2000
+        },
+        explanation: explanation + ' Frekvensområdet 1850 - 2000 kHz er sekundært.'
     }, {
         id: 2,
-        question: 'Hva er allokeringen(e) for 80 meter-båndet?',
-        answer: '3500 - 3800 kHz',
+        question: 'Hva er allokeringen(e) for 80 meter-båndet i kHz?',
+        answer: {
+            start: 3500,
+            end: 3800
+        },
         explanation: explanation
     }, {
         id: 3,
-        question: 'Hva er allokeringen(e) for 60 meter-båndet?',
-        answer: '5260 - 5410 kHz',
+        question: 'Hva er allokeringen(e) for 60 meter-båndet i kHz?',
+        answer: {
+            start: 5260,
+            end: 5410
+        },
         explanation: explanation
     }, {
         id: 4,
-        question: 'Hva er allokeringen(e) for 40 meter-båndet?',
-        answer: '7000 - 7200 kHz',
+        question: 'Hva er allokeringen(e) for 40 meter-båndet i kHz?',
+        answer: {
+            start: 7000,
+            end: 7200
+        },
         explanation: explanation
     }, {
         id: 5,
-        question: 'Hva er allokeringen(e) for 30 meter-båndet?',
-        answer: '10100 - 10150 kHz',
+        question: 'Hva er allokeringen(e) for 30 meter-båndet i kHz?',
+        answer: {
+            start: 10100,
+            end: 10150
+        },
         explanation: explanation
     }, {
         id: 6,
-        question: 'Hva er allokeringen(e) for 20 meter-båndet?',
-        answer: '14000 - 14350 kHz',
+        question: 'Hva er allokeringen(e) for 20 meter-båndet i kHz?',
+        answer: {
+            start: 14000,
+            end: 14350
+        },
         explanation: explanation
     }, {
         id: 7,
-        question: 'Hva er allokeringen(e) for 17 meter-båndet?',
-        answer: '18068 - 18168 kHz',
+        question: 'Hva er allokeringen(e) for 17 meter-båndet i kHz?',
+        answer: {
+            start: 18068,
+            end: 18168
+        },
         explanation: explanation
     }, {
         id: 8,
-        question: 'Hva er allokeringen(e) for 15 meter-båndet?',
-        answer: '21000 - 21450 kHz',
+        question: 'Hva er allokeringen(e) for 15 meter-båndet i kHz?',
+        answer: {
+            start: 21000,
+            end: 21450
+        },
         explanation: explanation
     }, {
         id: 9,
-        question: 'Hva er allokeringen(e) for 12 meter-båndet?',
-        answer: '24740 - 24990 kHz, 24740 - 24890 er sekundært',
-        explanation: explanation
+        question: 'Hva er allokeringen(e) for 12 meter-båndet i kHz?',
+        answer: {
+            start: 24740,
+            end: 24990
+        },
+        explanation: explanation + ' Frekvensområdet 24740 - 24890 er sekundært.'
     }, {
         id: 10,
-        question: 'Hva er allokeringen(e) for 10 meter-båndet?',
-        answer: '28000 - 29700 kHz',
+        question: 'Hva er allokeringen(e) for 10 meter-båndet i kHz?',
+        answer: {
+            start: 28000,
+            end: 29700
+        },
         explanation: explanation
     }];
     return {
